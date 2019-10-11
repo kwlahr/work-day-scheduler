9560a5f7795d060a5770945d330bdf4a7a54f44b
kwlahr <kwlahr@gmail.com>
2019-10-11 17:50:42 -0600

    Added input fields to index.html

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var date = new Date($.now());
 var simpleDate = (date.toDateString());
-// var simpleDate = date.getDay() + ", " + (date.getMonth()+1) + "/" + date.getDate + "/" + date.getFullYear();
 console.log(date);
 $("#currentDay").text(simpleDate);
+
+var data = $(".inputGroupText").val();
+console.log(data);
+function insertText(){
+
+}
