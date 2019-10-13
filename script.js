a7e0daccc56f74dd8ff6c2f8b44d58ff1c86e50b
kwlahr <kwlahr@gmail.com>
2019-10-13 14:32:31 -0600

    able to console log value of first input field

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ var simpleDate = (date.toDateString());
 console.log(date);
 $("#currentDay").text(simpleDate);
 
-var timeBlocks = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"];
+var timeBlocks = document.getElementById(["hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"]);
 console.log(timeBlocks);
 $("button").on("click", function(){
     var data = $(".input-group-text").val();
-    // $("input.input-group-text").each(function(index){
-    // });
-    // var myStorage = window.localstorage;
+    $("input.input-group-text").each(function(index){
+    });
+    var myStorage = window.localstorage;
     // var data = $(timeBlocks[i]).val();
     // var data = $(this).val();
     console.log(data);
-    // localStorage.setItem(data, "text");
-    // var saved = localStorage.getItem(data);
+    localStorage.setItem(data, "text");
+    var saved = localStorage.getItem(data);
 });
